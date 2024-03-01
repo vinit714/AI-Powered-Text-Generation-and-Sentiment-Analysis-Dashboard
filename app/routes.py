e8e5ef2f111aa4299d1937fce5afa14a10e5d897
Vinit Mehra <vinitm2001@gmail.com>
2024-03-01 11:36:50 +0530

    update

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, redirect, url_for, flash, request
 from app import app
 from app.forms import AnalysisForm
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.tokenize import word_tokenize
 
 @app.route('/')
 def index():
@@ -13,16 +14,44 @@ def analyze():
     form = AnalysisForm()
     if form.validate_on_submit():
         text = form.text.data  # Get the text input from the form
-        sentiment = analyze_sentiment(text)  # Perform sentiment analysis
-        return render_template('result.html', text=text, sentiment=sentiment)
+        sentiment, sentiment_scores, major_words = analyze_sentiment(text)  # Perform sentiment analysis
+        return render_template('result.html', text=text, sentiment=sentiment, sentiment_scores=sentiment_scores, major_words=major_words)
     return render_template('index.html', form=form)
 
 def analyze_sentiment(text):
     sid = SentimentIntensityAnalyzer()
     scores = sid.polarity_scores(text)
+    
+    # Tokenize the text
+    words = word_tokenize(text)
+    
+    positive_words = []
+    negative_words = []
+    neutral_words = []
+    
+
+    for word in words:
+        word_score = sid.polarity_scores(word)
+        if word_score['compound'] >= 0.5:
+            positive_words.append(word)
+        elif word_score['compound'] <= -0.5:
+            negative_words.append(word)
+        else:
+            neutral_words.append(word)
+    
+
+    major_words = {
+        'Positive': positive_words,
+        'Negative': negative_words,
+        'Neutral': neutral_words
+    }
+    
+    # overall sentiment
     if scores['compound'] >= 0.05:
-        return 'Positive'
+        sentiment = 'Positive'
     elif scores['compound'] <= -0.05:
-        return 'Negative'
+        sentiment = 'Negative'
     else:
-        return 'Neutral'
+        sentiment = 'Neutral'
+    
+    return sentiment, scores, major_words
